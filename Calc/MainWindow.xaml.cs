951ba2e6ba58a65b60d55d98eb90f1a76ea4b6cc
Smak2025 <smak-80+2025@yandex.ru>
2026-02-18 13:57:40 +0300

    Добавьте файлы проекта.

---
+++ Calc/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Сброс", "Действие:", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Digit_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                string tag;
+                switch (btn.Tag)
+                {
+                    case "pm": tag = "Знак ±"; break;
+                    case ".": tag = "Знак \",\""; break;
+                    default: tag = $"Цифра {btn.Tag}"; break;
+                }
+                MessageBox.Show(tag, "Действие:", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
